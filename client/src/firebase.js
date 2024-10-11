a7fd72b4e0e1ed8b31dd09cc7dae8b58a1587604
reddy <dnithinreddy123@gmail.com>
2024-10-11 11:37:02 +0530

    Initial commit with React, Tailwind CSS, and template setup

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhaxgdaLqWoca633Tig6VT2HGYf0zupns",
+  authDomain: "reddyestate-55a54.firebaseapp.com",
+  projectId: "reddyestate-55a54",
+  storageBucket: "reddyestate-55a54.appspot.com",
+  messagingSenderId: "686304382291",
+  appId: "1:686304382291:web:a6e1e3d50f882531fd25a6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
